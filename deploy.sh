c4df7da227e099bbc66f988a7e0cb37d9b14a3d6
Maxi <herreramaxi@gmail.com>
2023-02-22 22:39:09 +0000

    install and use node 18.14.0

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash 
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 18.14.0
+nvm use 18.14.0
 pm2 stop example_app
 cd ExampleApplication/
 npm install
-pm2 start ./bin/www --name example_app --env=production
+pm2 start ./bin/www --name example_app
