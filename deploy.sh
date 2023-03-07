9e0600a46f8183bdb00fc764474b985d6d74d557
Maxi <herreramaxi@gmail.com>
2023-03-07 01:09:28 +0000

    modify ci/cd to pull and execute docker image

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash 
-sudo apt update && sudo apt install nodejs npm
-sudo npm install -g pm2
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 18.14.0
-nvm use 18.14.0
-pm2 stop example_app
-cd ExampleApplication/
-npm install
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
+
+if ["$CURRENT_INSTANCE"]
+then
+  docker rm $(docker stop $CURRENT_INSTANCE)
+fi
+
+docker pull $IMAGE_NAME
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+
+if ["$CONTAINER_EXISTS"] 
+then
+  docker rm node_app
+fi
+
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-pm2 start ./bin/www --name example_app
+docker cp ./privatekey.pem node_app:/privatekey.pem
+docker cp ./server.crt node_app:/server.crt
+docker start node_app
