327b3fdab42637e759c89d94fd27ef500118e321
Maxi <herreramaxi@gmail.com>
2023-03-07 01:22:20 +0000

    trying to fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash 
 CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
-
-if ["$CURRENT_INSTANCE"]
+if [ "$CURRENT_INSTANCE" ]
 then
   docker rm $(docker stop $CURRENT_INSTANCE)
 fi
 
 docker pull $IMAGE_NAME
-CONTAINER_EXISTS=$(docker ps -a | grep node_app)
 
-if ["$CONTAINER_EXISTS"] 
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+if [ "$CONTAINER_EXISTS" ] 
 then
   docker rm node_app
 fi
